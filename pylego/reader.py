96d256db529972c45e26f71e4d3b5b1b44591355
Ankit Vani <a@nevitus.org>
2019-03-26 11:03:09 -0400

    Added DatasetReader

--- pylego/reader.py
+++ pylego/reader.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+import collections
+
+from torch.utils import data
 
 
 class Reader(ABC):
@@ -7,8 +10,7 @@ class Reader(ABC):
         """Initialize the reader.
 
         splits - dict
-            A dictionary of {split_name: int} which maps the name of the split
-            to the number of samples in that split
+            A dictionary of {split_name: int} which maps the name of the split to the number of samples in that split
         """
         self.splits = splits
 
@@ -22,3 +24,26 @@ class Reader(ABC):
     def iter_batches(self, split_name, batch_size, shuffle=True, partial_batching=False, threads=1, epochs=1,
                      max_batches=-1):
         pass
+
+
+class DatasetReader(Reader):
+    """Creates a Reader wrapper around PyTorch datasets."""
+
+    def __init__(self, dataset_splits):
+        """dataset_splits is a dictionary of {split_name: Dataset}"""
+        self.dataset_splits = dataset_splits
+        super().__init__(collections.OrderedDict([(k, len(v)) for k, v in dataset_splits.items()]))
+
+    def process_batch(self, batch):
+        """Process batch before yielding from iter_batches."""
+        return batch
+
+    def iter_batches(self, split_name, batch_size, shuffle=True, partial_batching=False, threads=1, epochs=1,
+                     max_batches=-1):
+        loader = data.DataLoader(self.dataset_splits[split_name], batch_size=batch_size, shuffle=shuffle,
+                                 num_workers=threads, drop_last=not partial_batching)
+        for _ in range(epochs):
+            for i, batch in enumerate(loader):
+                yield self.process_batch(batch)
+                if i + 1 == max_batches:
+                    break
